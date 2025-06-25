0645bb0659d0d835d50f1ca35e7e1052f70d5c00
Luis Romero <luiseduromp@hotmail.com>
2025-06-25 22:36:21 +0200

    🦾 Added analytics to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Inconsolata, Raleway } from "next/font/google";
 import { Navbar } from "@/components/navbar/Navbar";
 import { Footer } from "@/components/shared/Footer"
@@ -36,6 +37,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
